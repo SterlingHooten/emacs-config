6e3fef82f6809e4d8d8279224ebffd51a27e3e7d
Emilio Lopes <eclig@gmx.net>
2011-11-02 12:32:40 +0100

    Added command `cycle-word-capitalization'.

--- init.el
+++ init.el
@@ -946,6 +946,27 @@ Return the new value of VAR."
    "Window displaying '%s' is not dedicated anymore.")
  (current-buffer)))
 
+;; see http://www.emacswiki.org/emacs/RotateWordCapitalization
+(defun cycle-word-capitalization ()
+  "*Change the capitalization of the current word.
+If the word under point is in lower case, capitalize it.  If it
+is in capitalized form, change it to upper case.  If it is in
+upper case, downcase it."
+  (interactive "*")
+  (let ((case-fold-search nil))
+    (save-excursion
+      (skip-syntax-backward "w")
+      (cond
+       ((looking-at-p "[[:lower:]]+")
+        (capitalize-word 1))
+       ;; ((looking-at-p "[[:upper:]][[:lower:]]+")
+       ;;  (upcase-word 1))
+       ;; ((looking-at-p "[[:upper:]]+")
+       ;;  (downcase-word 1))
+       (t
+        (downcase-word 1))))))
+(global-defkey "M-C" 'cycle-word-capitalization)
+
 (defun leo (word)
   (require 'thingatpt)
   (interactive (list 
