5b0560a03ff524510a9d0c6afb279c951442393f
Emílio Lopes <eclig@gmx.net>
2013-05-14 23:03:08 +0200

    reduce pitch of defaul font "DejaVu Sans Mono" to 14.

--- init.el
+++ init.el
@@ -42,7 +42,7 @@
   (setq line-number-display-limit-width 512)
 
   (add-to-list 'default-frame-alist '(cursor-type . box))
-  (add-to-list 'default-frame-alist `(font . ,(if running-nt "Consolas 12" "DejaVu Sans Mono-16")))
+  (add-to-list 'default-frame-alist `(font . ,(if running-nt "Consolas 12" "DejaVu Sans Mono-14")))
 
   (when (and (display-color-p)
              (require-soft 'solarized-definitions))
