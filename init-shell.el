7dbbacb1a7aba6893ee3a72165243364e371dd55
Emilio Lopes <eclig@gmx.net>
2014-01-28 17:38:33 +0100

    Reorganised the whole mess, removing some cruft.

--- init-shell.el
+++ init-shell.el
@@ -1,6 +1,13 @@
-;; init-shell: Shell/Comint initialization
+;;; init-shell.el --- Shell/Comint initialization
 
-;; TODO: http://snarfed.org/why_i_run_shells_inside_emacs
+;; Author: Emílio Lopes <eclig@gmx.net>
+
+;; TODO: 
+;;
+;; * Work through http://snarfed.org/why_i_run_shells_inside_emacs
+
+
+;;; General setup
 
 (setq comint-use-prompt-regexp nil)
 (setq comint-use-prompt-regexp-instead-of-fields nil)
@@ -10,20 +17,33 @@
 ;; still need this correctly set.
 (setq shell-prompt-pattern "^[^\n]*[$#] ")
 
-(setq shell-completion-fignore '("~" "#" "%"))
-(setq comint-completion-autolist t)
-
-(setq comint-input-ring-size 4000)
-(setq comint-input-autoexpand nil)
-(setq shell-input-autoexpand nil)
-(setq comint-input-ignoredups t)
-(setq comint-buffer-maximum-size (* 8 1024))
-
 (setq comint-process-echoes nil)
 
-(setq comint-input-ignored-regexps
-      (list (rx bos (* (syntax whitespace)) eos)
-            (rx bos (or "y" "yes" "n" "no" "j" "ja" "nein") eos)))
+(setq comint-buffer-maximum-size (* 8 1024))
+
+;; (add-hook 'shell-mode-hook 'compilation-shell-minor-mode)
+
+(defun shell= (name)
+  (let* ((process (get-buffer-process (current-buffer)))
+         (shell (and process
+                     (file-name-sans-extension
+                      (file-name-nondirectory
+                       (car (process-command process)))))))
+    (and (stringp shell) (string= shell name))))
+
+(fset 'sh 'shell)
+
+
+;;; History and input filters
+
+(setq shell-input-autoexpand nil)
+(setq comint-input-autoexpand nil)
+(setq comint-input-ring-size 4000)
+(setq comint-input-ignoredups t)
+
+(defvar comint-input-ignored-regexps
+  (list (rx bos (* (syntax whitespace)) eos)
+        (rx bos (or "y" "yes" "n" "no" "j" "ja" "nein") eos)))
 
 (setq comint-input-filter
       (lambda (str)
@@ -33,21 +53,92 @@
              (and (string-match-p rx str)
                   (throw 'matched t)))))))
 
-;; (add-hook 'shell-mode-hook 'compilation-shell-minor-mode)
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (when (shell= "bash")
+              (setq comint-input-ring-file-name
+                    (or (getenv "HISTFILE")
+                        (if (file-exists-p "~/.bash.d/.bash_history")
+                            "~/.bash.d/.bash_history"
+                          "~/.bash_history")))
+              (comint-read-input-ring 'silent))))
+
+(when (require-soft 'helm-shell-history)
+  (defkey comint-mode-map "C-c C-l" 'comint-helm-input-ring))
+
+
+;;; Output filters
+
+(setq-default comint-scroll-show-maximum-output t)
+(setq-default comint-scroll-to-bottom-on-input 'this)
+
+(defun comint-postoutput-scroll-to-bottom* (_string)
+  "Like `comint-postoutput-scroll-to-bottom' but ignores the minibuffer when considering `comint-move-point-for-output'."
+  (let* ((current (current-buffer))
+	 (process (get-buffer-process current)))
+    (unwind-protect
+	(cond
+	 ((null process))
+	 ((bound-and-true-p follow-mode)
+	  (follow-comint-scroll-to-bottom))
+	 (t
+	  (let* ((this-window (selected-window))
+                 ;; `selected' is the selected window, unless we are in
+                 ;; the minibuffer, in which case it is the window
+                 ;; that was selected as the minibuffer was entered.
+                 (selected (if (minibuffer-window-active-p this-window) 
+                               (minibuffer-selected-window)
+                             this-window))) 
+	    (dolist (w (get-buffer-window-list current nil t))
+	      (select-window w)
+	      (unwind-protect
+		  (progn
+		    (comint-adjust-point selected)
+		    ;; Optionally scroll to the bottom of the window.
+		    (and comint-scroll-show-maximum-output
+			 (eobp)
+			 (recenter (- -1 scroll-margin))))
+		(select-window this-window))))))
+      (set-buffer current))))
+
+;; added the "for '[^']+'" bits for git
+(setq comint-password-prompt-regexp
+      (concat
+       "\\(^ *\\|"
+       (regexp-opt
+        '("Enter" "enter" "Enter same" "enter same" "Enter the" "enter the"
+          "Old" "old" "New" "new" "'s" "login"
+          "Kerberos" "CVS" "UNIX" " SMB" "LDAP" "[sudo]" "Repeat" "Bad") t)
+       " +\\)"
+       (regexp-opt
+        '("password" "Password" "passphrase" "Passphrase"
+          "pass phrase" "Pass phrase" "Response"))
+       "\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?\
+\\(?: for [^:]+\\| for '[^']+'\\)?:\\s *\\'"))
+
+(add-hook 'comint-mode-hook
+          (lambda ()
+            (setq comint-scroll-to-bottom-on-output 'others)
+            (when (shell= "bash")
+              (add-hook 'comint-preoutput-filter-functions 'shell-filter-ctrl-a-ctrl-b))
+            (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt)
+            (add-hook 'comint-output-filter-functions 'comint-postoutput-scroll-to-bottom*)
+            (add-hook 'comint-output-filter-functions 'comint-truncate-buffer)))
+
+
+;;; Completion
+
+(setq shell-completion-fignore '("~" "#" "%"))
+(setq comint-completion-autolist t)
 
 (add-to-path 'load-path (concat user-emacs-directory "lib/bacom"))
 
-(defun shell-bash-setup ()
-  "*Bash specific setup for `shell-mode-hook'."
-  (add-hook 'comint-preoutput-filter-functions 'shell-filter-ctrl-a-ctrl-b)
-  (setq comint-input-ring-file-name
-        (or (getenv "HISTFILE")
-            (if (file-exists-p "~/.bash.d/.bash_history")
-                "~/.bash.d/.bash_history"
-              "~/.bash_history")))
-  (comint-read-input-ring 'silent)
-  (when (require-soft 'bacom)
-    (add-hook 'completion-at-point-functions 'bacom-dynamic-complete nil t)))
+(add-hook 'shell-mode-hook
+          (lambda ()
+            (when (and (shell= "bash")
+                       (require-soft 'bacom))
+              (remove-hook 'completion-at-point-functions 'comint-completion-at-point t)
+              (add-hook 'completion-at-point-functions 'bacom-dynamic-complete nil t))))
 
 (defun comint-complete-from-history ()
   "*Complete symbol at point from history entries."
@@ -74,24 +165,28 @@
           (lambda ()
             (add-hook 'completion-at-point-functions 'comint-complete-from-history 'append t)))
 
-;; In a perfect world, we would add this function to
+(require-soft 'pcmpl-ack)
+
+;; In a perfect world we would just add the snippet bellow to
 ;; `shell-mode-hook'.  But as of Emacs 24.3 "hippie-expand" has a bug
 ;; so that making `hippie-expand-ignore-buffers' and
 ;; `hippie-expand-only-buffers' buffer-local does not have the desired
 ;; effect.
-
-;; (defun shell-setup-hippie-expand ()
-;;   (set (make-local-variable 'hippie-expand-verbose) nil)
-;;   (set (make-local-variable 'hippie-expand-dabbrev-as-symbol) t)
-;;   (set (make-local-variable 'hippie-expand-only-buffers) '(shell-mode))
-;;   (set (make-local-variable 'hippie-expand-try-functions-list)
-;;        '(try-expand-dabbrev
-;;          try-expand-dabbrev-all-buffers
-;;          try-complete-file-name-partially
-;;          try-complete-file-name)))
+;;
+;; (add-hook 'shell-mode-hook
+;;           (lambda ()
+;;             (set (make-local-variable 'hippie-expand-verbose) nil)
+;;             (set (make-local-variable 'hippie-expand-dabbrev-as-symbol) t)
+;;             (set (make-local-variable 'hippie-expand-only-buffers) '(shell-mode))
+;;             (set (make-local-variable 'hippie-expand-try-functions-list)
+;;                  '(try-expand-dabbrev
+;;                    try-expand-dabbrev-all-buffers
+;;                    try-complete-file-name-partially
+;;                    try-complete-file-name))))
 
 (defun shell-hippie-expand (arg)
   (interactive "P")
+  (require 'hippie-exp)
   (let ((hippie-expand-verbose nil)
         (hippie-expand-dabbrev-as-symbol t)
         (hippie-expand-only-buffers '(shell-mode))
@@ -106,18 +201,28 @@
           (lambda ()
             (define-key shell-mode-map [remap hippie-expand] 'shell-hippie-expand)))
 
-(add-hook 'shell-mode-hook
-          (lambda ()
-            (let ((shell (file-name-sans-extension
-                          (file-name-nondirectory
-                           (car (process-command (get-buffer-process (current-buffer))))))))
-              (when (string= shell "bash")
-                (shell-bash-setup)))))
+
+;;; Abbreviations
+
+(define-abbrev shell-mode-abbrev-table ",g" "| grep")
+(define-abbrev shell-mode-abbrev-table ",gi" "| grep -i")
+(define-abbrev shell-mode-abbrev-table ",gg" "2>&1 | grep")
+(define-abbrev shell-mode-abbrev-table ",s" "| sort ")
+(define-abbrev shell-mode-abbrev-table ",sn" "| sort --numeric-sort")
+(define-abbrev shell-mode-abbrev-table ",wcl" " | wc --lines")
+(define-abbrev shell-mode-abbrev-table ",nul" "/dev/null")
+(define-abbrev shell-mode-abbrev-table ",nn" "> /dev/null 2>&1")
+(define-abbrev shell-mode-abbrev-table ",t" "| tee")
+(define-abbrev shell-mode-abbrev-table ",tt" "2>&1| tee")
+(define-abbrev shell-mode-abbrev-table ",T" "| tail")
+(define-abbrev shell-mode-abbrev-table ",H" "| head")
+(define-abbrev shell-mode-abbrev-table ",h" "--help")
+(define-abbrev shell-mode-abbrev-table ",v" "--verbose")
+(define-abbrev shell-mode-abbrev-table ",V" "--version")
+(define-abbrev shell-mode-abbrev-table ",x" "| xargs")
 
 (add-hook 'shell-mode-hook
           (lambda ()
-            (setq comint-scroll-to-bottom-on-output 'others)
-            
             ;; sanitise the syntax table
             (modify-syntax-entry ?\, ".") ; punctuation
             (modify-syntax-entry ?\= ".") ; punctuation
@@ -136,46 +241,26 @@
             (abbrev-table-put shell-mode-abbrev-table :case-fixed t)
             (abbrev-mode 1)))
 
-(defmacro define-shell-abbrev (abbrev expansion)
-  "*Define ABBREV to expand to EXPANSION in `shell-mode'."
-  `(define-abbrev shell-mode-abbrev-table ,abbrev ,expansion))
-
-(define-shell-abbrev ",g" "| grep")
-(define-shell-abbrev ",gi" "| grep -i")
-(define-shell-abbrev ",gg" "2>&1 | grep")
-(define-shell-abbrev ",s" "| sort ")
-(define-shell-abbrev ",sn" "| sort --numeric-sort")
-(define-shell-abbrev ",wcl" " | wc --lines")
-(define-shell-abbrev ",nul" "/dev/null")
-(define-shell-abbrev ",nn" "> /dev/null 2>&1")
-(define-shell-abbrev ",t" "| tee")
-(define-shell-abbrev ",tt" "2>&1| tee")
-(define-shell-abbrev ",T" "| tail")
-(define-shell-abbrev ",H" "| head")
-(define-shell-abbrev ",h" "--help")
-(define-shell-abbrev ",v" "--verbose")
-(define-shell-abbrev ",V" "--version")
-(define-shell-abbrev ",x" "| xargs")
-
-(fset 'sh 'shell)
-
-(defun shell-setup-keys ()
-  "*Key bindings for shell-mode."
-  (defkey shell-mode-map "C-x C-r" 'view-file))
-
-(add-hook 'shell-mode-hook 'shell-setup-keys)
+
+;;; Colors in shell buffers
 
 (ansi-color-map-update 'ansi-color-names-vector
                        ["black" "red2" "green3" "orange" "blue3" "magenta2" "cyan3" "white"])
 
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-;; (add-hook 'shell-mode-hook 'pcomplete-shell-setup)
+
+
+;;; Directory tracking
 
 (when (require-soft 'pwdsync)
   (add-hook 'shell-mode-hook (lambda () (pwdsync-mode 1))))
 
 (setq shell-dirtrack-verbose nil)
 
+
+;;; Interaction between Emacs and the shell
+
+;; emacsclient --eval "(with-buffer-hosting-pid $$ (comint-read-input-ring))"
 (defmacro with-buffer-hosting-pid (pid body)
   "*Execute BODY in the buffer associated with the process with id PID.
 Return NIL if there is no buffer hosting a process with PID.
@@ -191,50 +276,8 @@ Otherwise return the value of the last form in BODY."
             (with-current-buffer (process-buffer ,proc)
               ,body)))))
 
-(when (require-soft 'helm-shell-history)
-  (defkey comint-mode-map "C-c C-l" 'comint-helm-input-ring))
-
 
-;;; http://www.masteringemacs.org/articles/2012/01/16/pcomplete-context-sensitive-completion-emacs/
-(defconst pcmpl-svn-commands
-  '("add" "blame" "praise" "annotate" "ann"
-    "cat" "changelist" "cl" "checkout" "co"
-    "cleanup" "commit" "ci" "copy" "cp"
-    "delete" "del" "remove" "rm" "diff" "di"
-    "export" "help" "h" "import" "info"
-    "list" "ls" "lock" "log" "merge" "mergeinfo"
-    "mkdir" "move" "mv" "rename" "ren"
-    "propdel" "pdel" "pd" "propedit" "pedit" "pe"
-    "propget" "pget" "pg" "proplist" "plist" "pl"
-    "propset" "pset" "ps"
-    "resolve" "resolved" "revert"
-    "status" "stat" "st" "switch" "sw"
-    "unlock" "update" "up" "upgrade"))
-
-(defun pcomplete/svn ()
-  "Completion for `svn'."
-  ;; Completion for the command argument.
-  (pcomplete-here* pcmpl-svn-commands)
-  (cond
-   ;; complete files/dirs forever if the command is `add' or `rm'.
-   ((pcomplete-match (regexp-opt '("add" "delete" "del" "remove" "rm")) 1)
-    (while (pcomplete-here (pcomplete-entries))))
-   ((pcomplete-match "ls" 1)
-    (let ((current (nth pcomplete-index pcomplete-args)))
-      (pcomplete-here* (pcmpl-svn-get-files (or (file-name-directory current) "."))
-                       (file-name-nondirectory current))))
-   (t
-    (while (pcomplete-here (pcomplete-entries))))))
-
-(defun pcmpl-svn-get-files (dir)
-  "Return a list of `svn' files in DIR."
-  (split-string (pcomplete-process-result "svn" "--non-interactive" "ls" dir)))
-
-(require-soft 'pcmpl-ack)
-
-;;; Comint
-(setq-default comint-scroll-to-bottom-on-input 'this)
-(setq-default comint-scroll-show-maximum-output t)
+;;; Manipulating the contents of the shell buffer
 
 ;; http://www.emacswiki.org/emacs/ShellMode
 (defun comint-clear-buffer ()
@@ -281,70 +324,20 @@ Otherwise return the value of the last form in BODY."
 ;;     (goto-char (point-at-bol)))
 ;;   ())
 
+
+;;; Miscellaneuous keybindings
 
-;; (defun comint-insert-last-word ()
-;;   "*Insert the last word from the previous history event at the cursor position."
-;;   (interactive)
-;;   (let (comint-input-ring-index)
-;;     (insert (comint-arguments (comint-previous-input-string 0) nil nil))))
+(add-hook 'comint-mode-hook
+          (lambda ()
+            (defkey comint-mode-map "M-P" 'comint-previous-matching-input-from-input)
+            (defkey comint-mode-map "M-N" 'comint-next-matching-input-from-input)
 
-(defun comint-setup ()
-  "*Setup for comint-mode."
-  (defkey comint-mode-map "M-P" 'comint-previous-matching-input-from-input)
-  (defkey comint-mode-map "M-N" 'comint-next-matching-input-from-input)
+            (defkey comint-mode-map "C-M-p" 'comint-previous-prompt)
+            (defkey comint-mode-map "C-M-n" 'comint-next-prompt)
 
-  (defkey comint-mode-map "C-M-p" 'comint-previous-prompt)
-  (defkey comint-mode-map "C-M-n" 'comint-next-prompt)
+            (defkey comint-mode-map "M-." 'comint-insert-previous-argument)))
 
-  (defkey comint-mode-map "M-." 'comint-insert-previous-argument)
-  (add-hook 'comint-output-filter-functions 'comint-watch-for-password-prompt)
-  (add-hook 'comint-output-filter-functions 'comint-postoutput-scroll-to-bottom*)
-  (add-hook 'comint-output-filter-functions 'comint-truncate-buffer))
-
-(add-hook 'comint-mode-hook 'comint-setup)
-
-(defun comint-postoutput-scroll-to-bottom* (_string)
-  "Like `comint-postoutput-scroll-to-bottom' but ignores the minibuffer when considering `comint-move-point-for-output'."
-  (let* ((current (current-buffer))
-	 (process (get-buffer-process current)))
-    (unwind-protect
-	(cond
-	 ((null process))
-	 ((bound-and-true-p follow-mode)
-	  (follow-comint-scroll-to-bottom))
-	 (t
-	  (let* ((this-window (selected-window))
-                 ;; `selected' is the selected window, unless we are in
-                 ;; the minibuffer, in which case it is the window
-                 ;; that was selected as the minibuffer was entered.
-                 (selected (if (minibuffer-window-active-p this-window) 
-                               (minibuffer-selected-window)
-                             this-window))) 
-	    (dolist (w (get-buffer-window-list current nil t))
-	      (select-window w)
-	      (unwind-protect
-		  (progn
-		    (comint-adjust-point selected)
-		    ;; Optionally scroll to the bottom of the window.
-		    (and comint-scroll-show-maximum-output
-			 (eobp)
-			 (recenter (- -1 scroll-margin))))
-		(select-window this-window))))))
-      (set-buffer current))))
-
-;; added the "for '[^']+'" bits for git
-(setq comint-password-prompt-regexp
-      (concat
-       "\\(^ *\\|"
-       (regexp-opt
-        '("Enter" "enter" "Enter same" "enter same" "Enter the" "enter the"
-          "Old" "old" "New" "new" "'s" "login"
-          "Kerberos" "CVS" "UNIX" " SMB" "LDAP" "[sudo]" "Repeat" "Bad") t)
-       " +\\)"
-       (regexp-opt
-        '("password" "Password" "passphrase" "Passphrase"
-          "pass phrase" "Pass phrase" "Response"))
-       "\\(?:\\(?:, try\\)? *again\\| (empty for no passphrase)\\| (again)\\)?\
-\\(?: for [^:]+\\| for '[^']+'\\)?:\\s *\\'"))
+
 
 (provide 'init-shell)
+;;; init-shell.el ends here
