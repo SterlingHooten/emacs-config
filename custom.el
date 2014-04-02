a429138441a3a823829bbae1a4bdb70a75a886b8
Emilio Lopes <eclig@gmx.net>
2014-04-02 17:25:02 +0200

    Updated `safe-local-variable-values'.

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((auto-insert-alist ((latex-mode . "LaTeX letter template") lambda nil (when (file-exists-p "template.tex") (insert-file-contents "template.tex")))) (dired-omit-mode . t) (after-save-hook . (lambda () (let ((b (org-export-as-html 3))) (and (bufferp b) (kill-buffer b)))))))))
+ '(safe-local-variable-values (quote ((dired-omit-extensions "~" ".bak" ".html" ".dir-locals.el" ".git/") (auto-insert-alist ((latex-mode . "LaTeX letter template") lambda nil (when (file-exists-p "template.tex") (insert-file-contents "template.tex")))) (dired-omit-mode . t) (after-save-hook lambda nil (let ((b (org-export-as-html 3))) (and (bufferp b) (kill-buffer b))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
