2df8c50d52853c1322420dc0b7c3ff833e0eafd5
Emilio Lopes <eclig@gmx.net>
2014-04-02 16:40:07 +0200

    Updated `safe-local-variable-values'.

--- custom.el
+++ custom.el
@@ -1,12 +1,12 @@
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((dired-omit-mode . t)))))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values (quote ((auto-insert-alist ((latex-mode . "LaTeX letter template") lambda nil (when (file-exists-p "template.tex") (insert-file-contents "template.tex")))) (dired-omit-mode . t) (after-save-hook . (lambda () (let ((b (org-export-as-html 3))) (and (bufferp b) (kill-buffer b)))))))))
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
